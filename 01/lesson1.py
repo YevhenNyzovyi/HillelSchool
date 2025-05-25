4fee62035596d9f079208059af80814e10b1fcc5
nizerman <nyzovoy@gmail.com>
2025-05-25 11:59:58 +0300

    second commit

--- 01/lesson1.py
+++ 01/lesson1.py
@@ -1,1 +1,11 @@
 print ('hello,world')
+texts = [
+    "Привіт, як справи?",         # Українська
+    "Hello, how are you?",        # Англійська
+    "Bonjour, comment ça va?"     # Французька
+]
+from langdetect import detect
+
+for text in texts:
+    print(f"'{text}' -> {detect(text)}")
+    print('hello,worlddddd')
